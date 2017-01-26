a3ea145d2c579def761dccd891350f1909c3ad88
beehyyv_dasith <dasithkuruppu@gmail.com>
2017-01-26 16:38:53 +0530

    underlaying library changed to reflect date parse

--- benchmark.js
+++ benchmark.js
@@ -1,9 +1,8 @@
 var pathToMongodb = require('./');
 
-var count = 100000;
+var count = 1;
 
-console.time('100000 times');
+console.time('1 times');
 while (count--) {
-  pathToMongodb('/posts/:year/:skip/:limit', '/posts/2015/200/100?(price=0.99||price="1.99")&&(sale=true||qty<20)', {defaultDB: 'test'});
+  pathToMongodb('/posts/:year/:skip/:limit', '/posts/2015/200/100?deliveryDate[$gte]=2015-01-01T00:00:00.000Z', {defaultDB: 'test'});
 }
-console.timeEnd('100000 times');
